95c5fa99a1cd06afc913e1a33874e69f773561aa
ToastBreadMan <xghostx2709@gmail.com>
2021-10-06 20:05:18 +0200

    3d movment without rotation

--- src/main.rs
+++ src/main.rs
@@ -36,42 +36,67 @@ fn main() {//use vector
         Pixels::new(WIDTH, HEIGHT, surface_texture).unwrap()
     };
     let rgba = [0,0xff,0,0xff];
+    let mut camera_pos = arr1(&[0.0,0.0,0.0]);
     event_loop.run(move |event, _, control_flow| {
         let now = Instant::now();
-        //a += 0.2;
+        //a += 0.02;
         //fov += 0.1;
-        for i in 0..points.len(){
-            points[i][0] += 1.0;
-        }
+
         let z_near = ((WIDTH as f32/2.0)/((fov/2.0) * PI / 180.0).tan() as f32) as f32;
         let angle = a * (PI/180.0);
         let rotate_x = ndarray::arr2(&[[1.0,0.0,0.0], [0.0, angle.cos(), -angle.sin()], [0.0, angle.sin(), angle.cos()]]);
-        for i in 0..points.len() {
-            points[i] =rotate_x.dot(&points[i]);
-        }
         *control_flow = ControlFlow::Poll;
-        for (i, vec) in points.iter().enumerate(){
-            let x_t = points[i][0] + 2.0;
-            let y_t = points[i][1] + 2.0;
-            let z_t = points[i][2] + 2.0;
-            let x = x_t as f32 * z_near/(z_near+z_t as f32);
-            let y = y_t as f32 * z_near/(z_near+z_t as f32);//test
-            //let new_vert = matrix.dot(&arr1(&[x as isize,y as isize,points[i][2] as isize]));
-            //println!("{}, {}, {}", points[i][0],points[i][1], points[i][2]);
-            //println!("x: {}, y:{}, z:{}", x,y, z_t);//MAKE SOMETHING BIGGER
-            world.add(Box::new(Circle::new(Vec2d{x:(x + 300.0) as isize, y:((y+ 300.0)) as isize}, 5.0, Box::new(rgba)))); //translate center
-        } 
-        //println!("-------------");
-        world.clear(pixels.get_frame());
-        world.update(pixels.get_frame());
-        pixels.render().unwrap();
+        if let Event::RedrawRequested(_) = event {
+            for (i, vec) in points.iter().enumerate(){ 
+                let x_t = points[i][0] + 2.0;
+                let y_t = points[i][1] + 2.0;
+                let z_t = points[i][2] + 2.0;
+                let x = x_t as f32 * z_near/(z_near+z_t as f32);
+                let y = y_t as f32 * z_near/(z_near+z_t as f32);//test
+                //let new_vert = matrix.dot(&arr1(&[x as isize,y as isize,points[i][2] as isize]));
+                //println!("{}, {}, {}", points[i][0],points[i][1], points[i][2]);
+                //println!("x: {}, y:{}, z:{}", x,y, z_t);//MAKE SOMETHING BIGGER
+                world.add(Box::new(Circle::new(Vec2d{x:(x + 300.0) as isize, y:((y+ 300.0)) as isize}, 5.0, Box::new(rgba)))); //translate center
+            } 
+            //println!("-------------");
+            world.clear(pixels.get_frame());
+            world.update(pixels.get_frame());
+            pixels.render().unwrap();
+            println!("{}", 1000/now.elapsed().as_millis());
+        }
         match event {
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 window_id,
             } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            Event::WindowEvent {
+                event:WindowEvent::KeyboardInput { input, .. },
+                window_id
+            } => {
+                let mut curr_pos = arr1(&[0.0,0.0,0.0]);
+                match input.virtual_keycode.unwrap() {
+                    winit::event::VirtualKeyCode::W => {
+                        curr_pos[2] += 1.
+                    },
+                    winit::event::VirtualKeyCode::S => {
+                        curr_pos[2] -= 1.
+                    },
+                    winit::event::VirtualKeyCode::A => {
+                        curr_pos[0] -= 1.
+                    }
+                    winit::event::VirtualKeyCode::D => {
+                        curr_pos[0] += 1.
+                    }
+                    _ => ()
+                }
+                for i in 0..points.len(){//only when movment
+                    for elem in 0..3 {
+                        points[i][elem] = &points[i][elem] - &curr_pos[elem];
+                    }
+                }
+            },
             _ => (),
         }
-        println!("{}", 1000/now.elapsed().as_millis());
+        window.request_redraw();
     });
 } 
