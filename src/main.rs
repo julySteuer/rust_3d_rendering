757373f1995243808605022581681bba776b397a
ToastBreadMan <xghostx2709@gmail.com>
2021-10-11 21:50:04 +0200

    added 3d matrix

--- src/main.rs
+++ src/main.rs
@@ -14,25 +14,35 @@ use models::{Rectangle::Rect, World::World, Circle::Circle, Polygon::Polygon, Ve
 fn main() {//use vector
     let WIDTH:u32 = 600;
     let HEIGHT:u32 = 600;
-    let fov = 45.0;
-    let mut points = vec![arr1(&[-100.0,-100.0,-100.0]),
-    arr1(&[100.0,-100.0,-100.0]),
-    arr1(&[100.0,100.0,-100.0]),
-    arr1(&[-100.0,100.0,-100.0]),
-    arr1(&[-100.0,-100.0,100.0]),
-    arr1(&[100.0,-100.0,100.0]),
-    arr1(&[100.0,100.0,100.0]),
-    arr1(&[-100.0,100.0,100.0])
+    let fov = 1.0 / (45.0/2.0 as f32).tan();
+    let far = 1000.0;
+    let near = 0.01;
+    let ar = WIDTH as f32/HEIGHT as f32;
+    let q = far/far-near;
+    let mut points:std::vec::Vec<ndarray::ArrayBase<ndarray::OwnedRepr<f64>, ndarray::Dim<[usize; 1]>>> = vec![arr1(&[-1.0,-1.0,-1.0,1.0]),
+    arr1(&[1.0,-1.0,-1.0,1.0]),
+    arr1(&[1.0,1.0,-1.0,1.0]),
+    arr1(&[-1.0,1.0,-1.0,1.0]),
+    arr1(&[-1.0,-1.0,1.0,1.0]),
+    arr1(&[1.0,-1.0,1.0,1.0]),
+    arr1(&[1.0,1.0,1.0,1.0]),
+    arr1(&[-1.0,1.0,1.0,1.0])
     ];
-    let mut points:std::vec::Vec<ndarray::ArrayBase<ndarray::OwnedRepr<f64>, ndarray::Dim<[usize; 1]>>> = vec![arr1(&[0.0,100.0,100.0]),
-    arr1(&[100.0,100.0,100.0]),
-    arr1(&[100.0,0.0,100.0]),
-    arr1(&[0.0,0.0,100.0]),
-    arr1(&[0.0,100.0,200.0]),
-    arr1(&[100.0,100.0,200.0]),
-    arr1(&[100.0,0.0,200.0]),
-    arr1(&[0.0,0.0,200.0])
-    ];//does maybe work
+    let projection_matrix = ndarray::arr2(&[
+    [ar as f64*fov as f64, 0.0, 0.0, 0.0], 
+    [0.0, fov as f64, 0.0, 0.0], 
+    [0.0,0.0,q,1.0], 
+    [0.0,0.0,(-far * near)/(far - near),0.0],
+    ]);
+    //let mut points:std::vec::Vec<ndarray::ArrayBase<ndarray::OwnedRepr<f64>, ndarray::Dim<[usize; 1]>>> = vec![arr1(&[0.0,100.0,100.0]),
+    //arr1(&[100.0,100.0,100.0]),
+    //arr1(&[100.0,0.0,100.0]),
+    //arr1(&[0.0,0.0,100.0]),
+    //arr1(&[0.0,100.0,200.0]),
+    //arr1(&[100.0,100.0,200.0]),
+   // arr1(&[100.0,0.0,200.0]),
+    //arr1(&[0.0,0.0,200.0])
+    //];//does maybe work
     //more testing
     let mut a = 10.0;
     let size = LogicalSize::new(WIDTH, HEIGHT);
@@ -46,27 +56,27 @@ fn main() {//use vector
     };
     let rgba = [0,0xff,0,0xff];
     let mut camera_pos = arr1(&[0.0,0.0,0.0]);
-    let z_0 = ((WIDTH/2) as f32/((fov/2.0) * PI / 180.0).tan() as f32) as f32; //make here bigger the hardcoded value i guess
+    //let z_0 = ((WIDTH/2) as f32/((fov/2.0) * PI / 180.0).tan() as f32) as f32; //make here bigger the hardcoded value i guess
     event_loop.run(move |event, _, control_flow| {
         let now = Instant::now();
         //fov += 0.1;
         let angle = a * (PI/180.0);
         let rotate_x = ndarray::arr2(&[[1.0,0.0,0.0], [0.0, angle.cos(), -angle.sin()], [0.0, angle.sin(), angle.cos()]]);
-        let rotate_y = ndarray::arr2(&[[angle.cos(), 0.0 , angle.sin()], [0.0,1.0,0.0], [-angle.sin(), 0.0, angle.cos()]]);
         //let cube_pos = arr1(&[0.0, 0.0, -20.0]);
         *control_flow = ControlFlow::Poll;
         if let Event::RedrawRequested(_) = event {
             for (i, vec) in points.iter().enumerate(){ 
-                let x_t = points[i][0];
-                let y_t = points[i][1];
-                let z_t = points[i][2];
-                let x = x_t as f32 * z_0/(z_0+z_t as f32);
-                let y = y_t as f32 * z_0/(z_0+z_t as f32);//rotate here maybe
+                let coords = projection_matrix.dot(&points[i]);
+                let x_t = coords[0] / coords[3];
+                let y_t = coords[1] / coords[3];
+                let z_t = coords[2] / coords[3];
+                //let x = ((coords[0] * WIDTH as f32) / (2.0 * WIDTH as f32) + WIDTH as f32 * 0.5)/coords[2];
+                //let y = ((coords[1] * HEIGHT as f32) / (2.0 * HEIGHT as f32) + HEIGHT as f32 * 0.5)/coords[2];
                 //let new_vert = matrix.dot(&arr1(&[x as isize,y as isize,points[i][2] as isize]));
                 //println!("{}, {}, {}", points[i][0],points[i][1], points[i][2]);
                 //println!("x: {}, y:{}, z:{}", x,y, z_t);//MAKE SOMETHING BIGGER
                 //if z_t < 0.0 {
-                    world.add(Box::new(Circle::new(Vec2d{x:(x + 300.0) as isize, y:((y+ 300.0)) as isize}, 5.0, Box::new(rgba)))); //translate center
+                    world.add(Box::new(Circle::new(Vec2d{x:(x_t + 300.0) as isize, y:((y_t+ 300.0)) as isize}, 5.0, Box::new(rgba)))); //translate center
                 //} 
             } 
             //println!("-------------");
@@ -87,23 +97,33 @@ fn main() {//use vector
                 let mut curr_pos = arr1(&[0.0,0.0,0.0]);
                 match input.virtual_keycode.unwrap() {
                     winit::event::VirtualKeyCode::W => {
-                        curr_pos[2] += 3.
+                        curr_pos[2] += 0.5
                     },
                     winit::event::VirtualKeyCode::S => {
-                        curr_pos[2] -= 3.
+                        curr_pos[2] -= 0.5
                     },
                     winit::event::VirtualKeyCode::A => {
-                        curr_pos[0] -= 3.
+                        curr_pos[0] -= 0.5
                     }
                     winit::event::VirtualKeyCode::D => {
-                        curr_pos[0] += 3.
+                        curr_pos[0] += 0.5
                     }
                     winit::event::VirtualKeyCode::C => { // rotate by the inverse
+                        let rotate_y = ndarray::arr2(&[[angle.cos(), 0.0 , angle.sin()], [0.0,1.0,0.0], [-angle.sin(), 0.0, angle.cos()]]);
                         //a += 0.1;
                         for i in 0..points.len() {
-                            points[i] = rotate_y.dot(&points[i]);//rotate then move by aprox z_0
+                            points[i] = rotate_y.dot(&arr1(&[points[i][0], points[i][1], points[i][2]]));//rotate then move by aprox z_0
+                            points[i] = arr1(&[points[i][0], points[i][1], points[i][2], 1.0]);
                         }
                     },
+                    winit::event::VirtualKeyCode::X => {
+                        let turning_angle = -angle;
+                        let rotate_y = ndarray::arr2(&[[-turning_angle.cos(), 0.0 , -turning_angle.sin()], [0.0,1.0,0.0], [turning_angle.sin(), 0.0, -turning_angle.cos()]]);
+                        for i in 0..points.len() {
+                            points[i] = rotate_y.dot(&arr1(&[points[i][0], points[i][1], points[i][2]]));//rotate then move by aprox z_0
+                            points[i] = arr1(&[points[i][0], points[i][1], points[i][2], 1.0]);
+                        }
+                    }
                     _ => ()
                 }
                 for i in 0..points.len(){
